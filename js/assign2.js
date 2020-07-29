5b000a27b4971094b471a5fa2000646c2da79ae4
naveen kumar <naveen_kumar@excellencetechnologies.in>
2020-07-29 13:49:34 +0530

    changes in assignment2 of selectors add and remove row

--- js/assign2.js
+++ js/assign2.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){ 
     $('.add').click(function() {
-        $('.main:last').before('<div class="main"><input type="text"> <button>Browse</button> <select><option>Property Image</option><option>Property Image 1</option></select> <a class="remove anchor" href="#">Remove</a><div>');
+        $('.main:last').before('<div class="main"><input type="file"> <select><option>secondary Image</option><option> Thrid Image</option><option>Fourth Image</option><option>Fifth Image</option></select> <a class="remove anchor" href="#">Remove</a><div>');
     });
     $('form').on('click','.remove',function() {
  	    $(this).parent().remove();
